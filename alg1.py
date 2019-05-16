9e0ab5ce742a9156c48753eaa61820700258ff6d
xparisax <parisa.khania@yahoo.com>
2019-05-17 00:10:14 +0430

    input filealg1.py

---
+++ alg1.py
@@ -0,0 +1,20 @@
+# import inputG, algrtm2, algrtm3
+
+my_file = list(open('graph_sample1.txt', 'r'))
+#read edges from file
+edge = []
+edges = []
+for line in my_file:
+	edge.append(line)
+	edge = [x.strip() for x in edge]
+for i in range(len(edge)):
+	u, v = edge[i].split()
+    edges[(int(u)] = int(v)
+    edges[(int(v)] = int(u)
+
+while len(Removed) <= k or #check edge with func:
+	v_star = #def in alg2 /3 : select v ∗ = arg min v∈V f (G \ {v ∗ })
+	# edges[v*] = 'null' always check if v star is in removed
+	Removed.add(v_star)
+
+
